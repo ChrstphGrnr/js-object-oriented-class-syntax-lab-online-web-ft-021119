1db77d2af66b77f4e0af6fbc354ae3b983449711
Christoph Gruener <christophgruener@me.com>
2019-05-28 17:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ class BoardMember{
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+
+class Ceo {
+  constructor(name, training, homestate) {
+    this.
+  }
+}
